543a660a5c50fed73de837d324543556078c6cf5
Taufik Sulaeman P <taufiksu@gmail.com>
2020-05-21 11:56:35 +0700

    Prepare Template

--- www/sample/basic/js_app/constant.js
+++
@@ -1,8 +0,0 @@
-var localStorage = require('localStorage');
-var { ipcRenderer } = require('electron');
-const electron = require('electron');
-const BrowserWindow = electron.remote.BrowserWindow;
-const remote = require('electron').remote;
-const path = require('path');
-const url = require('url');
-const fs = require('fs');
--- www/sample/basic/js_app/listener.js
+++
@@ -1,9 +0,0 @@
-$$(document).on('click', '#btn-close', function() {
-    var window = remote.getCurrentWindow();
-    window.close();
-});
-
-$$(document).on('click', '#btn-minimize', function() {
-    var window = remote.getCurrentWindow();
-    window.minimize();
-});
