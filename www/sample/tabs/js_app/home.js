dc24bfef9043ae67f56183edde83becda454bad0
Taufik Sulaeman P <taufiksu@gmail.com>
2020-05-21 22:39:29 +0700

    1. Tab View

--- www/sample/tabs/js_app/home.js
+++ www/sample/tabs/js_app/home.js
@@ -8,6 +8,18 @@ $$(document).on('page:afterin', '.page[data-name="home"]', function(callback) {
     }
 });
 
+$$(document).on('page:afterin', '.page[data-name="about"]', function(callback) {
+    console.log('about');
+});
+
+$$(document).on('tab:show', '#tab-home', function() {
+    $$(document).find('#page-title').html('Home');
+});
+
+$$(document).on('tab:show', '#tab-info', function() {
+    $$(document).find('#page-title').html('Info');
+});
+
 $$(document).on('click', '#btn-install', function() {
     deferredPrompt.prompt();
 
